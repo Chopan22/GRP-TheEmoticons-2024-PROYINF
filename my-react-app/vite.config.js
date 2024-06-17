17225262b6dd3b0cb2c7eec6eb83d12936d9ce6c
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2024-06-16 22:29:05 -0400

    casi al final

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { FsaNodeFs } from 'memfs/lib/fsa-to-node'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
   ],
 })
