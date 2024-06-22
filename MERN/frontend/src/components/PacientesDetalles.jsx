237fd75b1a2e67884614f03bd56980594c9d132c
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2024-06-22 00:34:11 -0400

    se añade un poco de css a la pagina de Pacientes

--- MERN/frontend/src/components/PacientesDetalles.jsx
+++ MERN/frontend/src/components/PacientesDetalles.jsx
@@ -1,16 +1,29 @@
 import './PacientesDetalles.css'
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
 const PacienteDetalles = ({ paciente}) => {
     return (
         <div className="pacientes-detalles">
             <div className="card"> 
 
-                <h4> { paciente.nombre}</h4>
+                <h2 className='nombre-pacientes'> { paciente.nombre}</h2>
                 <p><strong> Rut: </strong>{paciente.rut}</p>
-                <p><strong> fecha de nacimiento: </strong>{paciente.fecha_nacimiento}</p>
+                <p><strong> Fecha de Nacimiento: </strong>{formatDate(paciente.fecha_nacimiento)}</p>
                 <p><strong> Sexo: </strong>{paciente.sexo}</p>
-                <p><strong> telefono: </strong>{paciente.telefono}</p>
-                <p>{paciente.createdAt}</p>
+                <p><strong> Telefono: </strong>{paciente.telefono}</p>
+                <p><strong> Ingresado al Hospital: </strong>{formatDate(paciente.createdAt)}</p>
+                <div className="container">
+                    <button class="button-24" role="button"> Revisar Perfil </button>
+                </div>
+                
 
             </div>
             
