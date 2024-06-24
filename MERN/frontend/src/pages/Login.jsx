f0f1606695f164d73c56fb74e896dfb0f9a5eb38
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2024-06-24 01:42:09 -0400

    Oficialmente funciona el login, pero no modifica nada en la pagina web

--- MERN/frontend/src/pages/Login.jsx
+++ MERN/frontend/src/pages/Login.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react"
+import { usarLogin } from "../hooks/UsarLogin"
+
 
 const Login = () =>{
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const { login , error, isLoading} = usarLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await login(email, password)
     }
 
     return (
@@ -30,7 +33,8 @@ const Login = () =>{
                 value={password}
             />
 
-            <button> Sign Up</button>
+            <button disabled={isLoading}> Log in</button>
+            {error && <div classname="error"> {error} </div>}
         </form>
     )
 }
