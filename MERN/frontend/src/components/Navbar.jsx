6593da65229ecfbfe1844303fe55912271569747
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2024-06-24 01:34:09 -0400

    Se agrega la funcionabilidad de Logout

--- MERN/frontend/src/components/Navbar.jsx
+++ MERN/frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom'
-
+import { UsarLogout } from '../hooks/UsarLogout'
 import './Navbar.css'
 
 const Navbar = () => {
+    const { logout } = UsarLogout()
+
+    const handleClick = () => {
+        logout()
+    }
+
+
+
     return (
         <div className='nav'>
             <div className='nav-logo'>CodeConvergence</div>
@@ -11,6 +19,11 @@ const Navbar = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/pacientes">Pacientes</Link></li>
                 <input type="text" placeholder="Buscar Paciente" className="nav-search" />
+
+
+                <div>
+                    <button onClick={handleClick}>Log Out</button>
+                </div>
                 <li className='nav-login'> <Link to="/login"> Login </Link> </li>
                 <li className='nav-signup'> <Link to="/signup"> Signup </Link> </li>
             </ul>
