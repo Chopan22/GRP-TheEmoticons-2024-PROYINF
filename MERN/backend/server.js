fa276861938e3dc539702ccc19c06f259ba05ee1
mrojas32 <131390769+mrojas32@users.noreply.github.com>
2024-10-07 21:36:49 -0300

    Update server.js

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -1,44 +1,39 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-// console.log(process.env.MONGO_URI)
-
-// Conexion a la base de datos 
-const mongoose = require('mongoose')
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 
-const pacientesRoutes = require('./routes/pacientes')
-
-const usuariosRoutes = require('./routes/usuarios')
-
-
+const pacientesRoutes = require('./routes/pacientes');
+const usuariosRoutes = require('./routes/usuarios');
 
 // Crea la app express
-const app = express()
+const app = express();
 
-// MiddleWare
-app.use(express.json())
-app.use(cors())
+// Middleware
+app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
-})
+});
 
 
-app.use('/api/pacientes',pacientesRoutes)
-app.use('/api/usuarios',usuariosRoutes)
+
+app.use('/api/pacientes', pacientesRoutes);
+app.use('/api/usuarios', usuariosRoutes);
 
 // Conectarse a la BD 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log("Se conecto a la base de datos y ahora Escuchando en el puerto 4000");
-        })
+            console.log("Se conectó a la base de datos y ahora escuchando en el puerto", process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
 
 // Escuchando llamadas
 
