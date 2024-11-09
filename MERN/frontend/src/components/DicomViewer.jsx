2dae7f62fc4af1a48c0b446b2818cb61df9e5a6c
Francisco <121066531+Chopan22@users.noreply.github.com>
2024-11-09 20:50:54 -0300

    Update DicomViewer.jsx

--- MERN/frontend/src/components/DicomViewer.jsx
+++ MERN/frontend/src/components/DicomViewer.jsx
@@ -1,34 +1,50 @@
-import React, { useEffect, useRef } from 'react';
-import useDicom from '../hooks/UsarDicom';
-import cornerstone from 'cornerstone-core';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { UsarAuthContexto } from '../hooks/UsarAuthContexto';
 
-const DicomViewer = ({ dicomId }) => {
-  const { dicomData, metadata, loading } = useDicom(dicomId);
-  const dicomElement = useRef(null);
+//Aun no funciona esto ########################################
 
-  useEffect(() => {
-    if (dicomData && dicomElement.current) {
-      cornerstone.enable(dicomElement.current);
-      cornerstone.loadImage(dicomData).then((image) => {
-        cornerstone.displayImage(dicomElement.current, image);
+const DicomViewer = () => {
+  const [images, setImages] = useState([]);
+  const { user } = UsarAuthContexto();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchImages = async () => {
+    try {
+        const response = await axios.get('http://localhost:4000/api/upload/images',{
+          headers: {
+              'Authorization': `Bearer ${user.token}`
+          }
       });
+        // Check if response contains images
+        if (Array.isArray(response.data)) {
+            setImages(response.data); // Set images from response
+        } else {
+            console.error("Unexpected response format:", response.data);
+            setError("Unexpected response format");
+        }
+    } catch (error) {
+        console.error("Error fetching images:", error);
+        setError("Failed to load images"); // Set error message
+    } finally {
+        setLoading(false); // Set loading to false after fetch completes
     }
-  }, [dicomData]);
+};
 
-  if (loading) return <p>Loading...</p>;
+  useEffect(() => {   
+      fetchImages();
+  }, []);
 
   return (
-    <div>
-      <h2>DICOM Viewer</h2>
-      {metadata && (
-        <div>
-          <p>Patient Name: {metadata.patientName}</p>
-          <p>Study Date: {metadata.studyDate}</p>
-          <p>Modality: {metadata.modality}</p>
-        </div>
-      )}
-      <div ref={dicomElement} style={{ width: '512px', height: '512px', border: '1px solid black' }}></div>
-    </div>
+      <div>
+          <h2>Uploaded DICOM Images</h2>
+          <ul>
+              {images.map((image) => (
+                  <li key={image._id}>{image.data.data}</li>
+              ))}
+          </ul>
+      </div>
   );
 };
 
